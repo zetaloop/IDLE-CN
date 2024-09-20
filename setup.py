96ca24aded4b1147cc1780faadea4386557c577c
zetaloop <zetaloop@outlook.com>
2024-09-20 16:02:04 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def find_idcn_release_files():
 
 setup(
     name="idcn",
-    version="1.1.2",
+    version="1.2.0",
     author="zetaloop",
     author_email="zetaloop@outlook.com",
     description="IDLE Chinese Translation | IDLE 汉化包",
